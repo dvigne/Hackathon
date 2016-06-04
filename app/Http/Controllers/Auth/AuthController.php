f25c0bb5cf4f7b0c537c756526792d7face43e71
Derick Vigne <derickvigne@me.com>
2016-06-04 09:24:20 -0400

    Created Registration Page -- Modified Migrations For Database

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -49,7 +49,8 @@ class AuthController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'required|max:255',
+            'first' => 'required|max:255',
+            'last' => 'required|max:255',
             'email' => 'required|email|max:255|unique:users',
             'password' => 'required|min:6|confirmed',
         ]);
@@ -64,7 +65,8 @@ class AuthController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
+            'first' => $data['first'],
+            'last' => $data['last'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
