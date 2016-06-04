8086bf5096df8e82e4106e948d74a9ba4f88bb66
Derick Vigne <derickvigne@me.com>
2016-06-04 18:50:54 -0400

    Added Mail Messaging Ability

--- config/app.php
+++ config/app.php
@@ -147,6 +147,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Cmgmyr\Messenger\MessengerServiceProvider::class,
 
         /*
          * Application Service Providers...
